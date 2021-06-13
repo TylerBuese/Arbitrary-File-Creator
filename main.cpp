4644e09a9725a699c02b2d7061c39943d1cd7e22
TylerBuese <36856818+TylerBuese@users.noreply.github.com>
2021-06-12 22:23:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 
 int main()
 {
-    std::cout << "Please enter how large you'd like the file to be (in KB): ";
+    std::string location;
     int size;
+    std::string fileName;
+    std::cout << "Please enter the location of where you'd like to store the file (Note: Fully qualify your location. eg, add \\ to the end of the location.): ";
+    std::cin >> location;
+    std::cout << "\n";
+    std::cout << "Please name the file: ";
+    std::cin >> fileName;
+    std::cout << "\n";
+    std::cout << "Please enter how large you'd like the file to be (in KB): ";
     std::cin >> size;
+    std::cout << "\n";
     File file;
-    file.CreateFile("File", "C:\\Users\\Tyler\\Desktop\\New folder\\stuff.txt", size * 1024);
+    file.CreateFile(fileName, location, size * 1024);
     return 0;
 }
