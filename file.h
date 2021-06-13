23e3db2dc93fdc6339cba7544056f83c1b4a6ba1
TylerBuese <36856818+TylerBuese@users.noreply.github.com>
2021-06-12 22:25:48 -0500

    Added new options
    Execution now wipes file first, then adds data. You can now specify a location. You do not need to escape "\". You cannot have spaces in path (yet)

--- file.h
+++ file.h
@@ -1,24 +1,53 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
-class File 
+class File
 {
-    private:
+private:
     std::string FileName;
     std::string FileLocation;
     int FileSize;
 
-    
-    public:
-    void CreateFile(std::string name, std::string location, int size)
+    void WipeFile(std::string location)
     {
         std::ofstream file;
-        file.open(location, std::ios_base::app);
-        for (int i = 0; i < size; i++) 
+        file.open(location);
+        file << "";
+        file.close();
+    }
+
+public:
+    void CreateFile(std::string name, std::string location, int size)
+    {
+        if (size > 1073741824)
+        {
+            std::cout << "Unable to write file due to illegal size. Size chosen: " << size;
+            return;
+        }
+        std::string delimiter = "\\";
+        std::string token;
+        size_t pos = 0;
+        std::vector<std::string> strings;
+        while((pos = location.find(delimiter)) != std::string::npos)
+        {
+            token = location.substr(0, pos);
+            std::cout << token << "\n";
+            location.erase(0, pos + delimiter.length());
+            strings.push_back(token + "\\");
+        }
+        std::string NewFileLocaiton;
+        for (int i = 0; i < strings.size(); i++)
+        {
+            NewFileLocaiton += strings[i];
+        }
+        WipeFile(NewFileLocaiton + name);
+        std::ofstream file;
+        file.open(NewFileLocaiton + name, std::ios_base::app);
+        for (int i = 0; i < size; i++)
         {
             file << "0";
         }
         file.close();
     }
-
 };
